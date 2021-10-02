2e0dd542740aa68cfd8ed58afa7257a257e25e4c
cfcguerra <roVgyz-nofte0-cedpyc>
2021-10-02 22:19:50 +0100

    Added commands to constants.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,9 @@ public class Main {
     public static final String REMOVE_CONTACT = "RC";
     public static final String GET_PHONE      = "GP";
     public static final String GET_EMAIL      = "GE";
+    public static final String GET_NAME       = "GN";
     public static final String SET_PHONE      = "SP";
+    public static final String REPEATED_PHONES = "EP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
